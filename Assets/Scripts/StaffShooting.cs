dd553e63bf8b223c9e8b1de7b45155da5ca977f8
Jeffrey-Ruth <jeffmrjr22@gmail.com>
2024-03-07 18:29:52 -0600

    Staff added

---
+++ Assets/Scripts/StaffShooting.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class StaffShooting : MonoBehaviour
+{
+
+    public GameObject Bullet;
+    public float BulletForce = 100.0f;
+    public float destroyTime = 3.0f;
+    AudioSource myaudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.LeftControl) || Input.GetMouseButtonDown(0)))
+        {
+
+            //create a bullet instance
+            GameObject currentBullet = Instantiate(Bullet, this.transform.position, this.transform.rotation) as GameObject;
+
+            //fix scale
+            currentBullet.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+            //add force to shoot
+            currentBullet.GetComponent<Rigidbody>().AddForce(transform.forward * BulletForce);
+
+            myaudio.Play();
+
+            //Destroy it after a certain time
+            Destroy(currentBullet, destroyTime);
+        }
+    }
+}

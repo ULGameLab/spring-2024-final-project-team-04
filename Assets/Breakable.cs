f0ce4b8db28e7d472055b8d43668a9d63d79f29a
stunfiskme <brennanhen@gmail.com>
2024-04-11 17:30:05 -0500

    objects can break in boss rooms now!

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+    int hitsTaken = 0;
     void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Attack"))
+        {
+            hitsTaken++;
+        }
+        if (hitsTaken >= 3)
+        {
+            if ((other.gameObject.CompareTag("Decor") || other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Attack"))
+            && hitsTaken == 3)
+            {
+                //Debug.Log("Trigger 1" + gameObject);
+                Destroy(gameObject);
+            }
+        }
+        if (other.gameObject.CompareTag("Spider")) {
+            Destroy(gameObject);
+        }
+    }
 
-        if (other.gameObject.CompareTag("Decor") || other.gameObject.CompareTag("Wall"))
-        {
-            //Debug.Log("Trigger 1" + gameObject);
-            Destroy(gameObject);
-        }
-    }
-    void OnTriggerStay(Collider other)
-    {
-       
-        if (other.gameObject.CompareTag("Decor") || other.gameObject.CompareTag("Wall"))
-        {
-            //Debug.Log("Trigger 2" + gameObject);
-            Destroy(gameObject);
-        }
-    }
     void OnCollisionEnter(Collision collision)
-
     {
-       
-        if (collision.gameObject.CompareTag("Decor") || collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Attack"))
+        if (collision.gameObject.CompareTag("Attack")) {
+            hitsTaken++;
+        }
+        if (hitsTaken >= 3)
         {
-            //Debug.Log("Trigger 3" + gameObject);
-            Destroy(gameObject);
+            if ((collision.gameObject.CompareTag("Decor") || collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Attack")))
+            {
+                //Debug.Log("Trigger 3" + gameObject);
+                Destroy(gameObject);
+            }
         }
     }
     
